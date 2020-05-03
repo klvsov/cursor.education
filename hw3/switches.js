7332646d2034d39c5f1c8160fd2cb957891920b9
klvsov <klvsov@gmail.com>
2020-05-03 21:24:12 +0300

    added imitation of label

--- hw3/switches.js
+++ hw3/switches.js
@@ -1,8 +1,8 @@
 window.onload = function(){
     let mainBox = document.querySelector('#box-Name');
     let boxes = document.querySelectorAll('.box-file');
-    
-
+    let name = document.querySelector('.name');
+    let fileName = document.querySelectorAll('.file-name');
 
     mainBox.addEventListener('click', function(){
         this.classList.toggle('box__active');
@@ -12,6 +12,21 @@ window.onload = function(){
             }
         }
         if(this.classList.contains('box__active')){
+            for(let i = 0; i < boxes.length; i++){
+                boxes[i].classList.add('box__active');
+            }
+        }
+        
+    })
+
+    name.addEventListener('click', function(){
+        mainBox.classList.toggle('box__active');
+        if(!mainBox.classList.contains('box__active')){
+            for(let i = 0; i < boxes.length; i++){
+                boxes[i].classList.remove('box__active');
+            }
+        }
+        if(mainBox.classList.contains('box__active')){
             for(let i = 0; i < boxes.length; i++){
                 boxes[i].classList.add('box__active');
             }
@@ -35,6 +50,23 @@ window.onload = function(){
             if(boxesItems.every(classOfCurrentElement)){
                 mainBox.classList.remove('box__active');
             }
+        })
+    }
+
+
+    for(let i = 0; i < fileName.length; i++){
+        fileName[i].addEventListener('click', function(){
+            boxes[i].classList.toggle('box__active');
+            if(boxes[i].classList.contains('box__active')){
+                mainBox.classList.add('box__active');
+            }
+
+            boxesItems = Array.from(boxes)
+            if(boxesItems.every(classOfCurrentElement)){
+                mainBox.classList.remove('box__active');
+            }
+
+            
         })
     }
 }
