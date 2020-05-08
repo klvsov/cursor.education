4f01d8182754c685aa77b5e9e4dc10205ac38616
klvsov <klvsov@gmail.com>
2020-05-08 22:40:28 +0300

    photo carousel done

--- hw5/scripts/slider.js
+++ hw5/scripts/slider.js
@@ -1,21 +1,25 @@
 window.onload = function(){
     let images = document.querySelectorAll('.save_img img');
     let mainImg = document.querySelector('.save_img-3 img');
-    let nextImg = document.querySelector('.save_img-1 img');
-    let prevImg = document.querySelector('.save_img-2 img');
-
-   
-
-    nextImg.addEventListener('click', function(){
-        let temp = mainImg.src;
-        mainImg.src = this.src;
-        this.src = temp;
-    })
+    let prevImg = document.querySelector('.save_img-1 img');
+    let nextImg = document.querySelector('.save_img-2 img');
 
     prevImg.addEventListener('click', function(){
-        let temp = mainImg.src;
-        mainImg.src = this.src;
-        this.src = temp;
+        let tempM = mainImg.src;
+        let tempL = prevImg.src;
+        let tempR = nextImg.src
+        mainImg.src = tempR;
+        prevImg.src = tempM;
+        nextImg.src = tempL;
+    })
+
+    nextImg.addEventListener('click', function(){
+        let tempM = mainImg.src;
+        let tempL = prevImg.src;
+        let tempR = nextImg.src
+        mainImg.src = tempL;
+        prevImg.src = tempR;
+        nextImg.src = tempM;
     })
     
 }
