ad0b526682720155a5ce5b6d17d5979f6250aadf
klvsov <klvsov@gmail.com>
2020-05-24 11:33:44 +0300

    fixed mistakes from Humeniuk Maryna and Serhii

--- hw9/scripts/slider.js
+++ hw9/scripts/slider.js
@@ -1,25 +1,60 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        center: true,
-        items:2,
-        loop:true,
-        margin:50,
-        autoplay:false,
-        smartSpeed:1000,
+    $('.carousel').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow:4,
+        arrows: true,
         dots: true,
-        nav:true,
-        autoplayTimeout:2000,
-        responsive:{
-            360:{
-                items:0
+        prevArrow: "<span class='prev'><img src='images/prev_arrow.svg' alt='prev'></span>",
+        nextArrow: "<span class='next'><img src='images/next_arrow.svg' alt='next'></span>",
+        responsive: [
+            {
+                breakpoint: 1430,
+                settings: {
+                  arrows: true,
+                  centerMode: true,
+                  centerPadding: '10px',
+                  slidesToShow: 4
+                }
             },
-            600:{
-                items:3
-            },
-            1000:{
-                items:4
-            },
-        }
-    });
-});
 
+            {
+                breakpoint: 1290,
+                settings: {
+                  arrows: true,
+                  centerMode: true,
+                  centerPadding: '10px',
+                  slidesToShow: 3
+                }
+            },
+
+            {
+                breakpoint: 995,
+                settings: {
+                  arrows: true,
+                  centerMode: true,
+                  centerPadding: '10px',
+                  slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 690,
+                settings: {
+                arrows: true,
+                centerMode: true,
+                centerPadding: '100px',
+                slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 540,
+                settings: {
+                arrows: true,
+                centerMode: true,
+                centerPadding: '10px',
+                slidesToShow: 1
+                }
+            }
+        ]
+      });
+  });
