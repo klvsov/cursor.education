f17c774df01fb52c253a58f0c259fcbf76ab5f15
klvsov <klvsov@gmail.com>
2020-06-10 12:12:36 +0300

    remove slick slider & create mini slider

--- hw14/lib/image.js
+++ hw14/lib/image.js
@@ -1,18 +1,30 @@
-window.onload = function () {
-    document.body.classList.add('loaded_hiding');
-    window.setTimeout(function () {
-      document.body.classList.add('loaded');
-      document.body.classList.remove('loaded_hiding');
-    }, 1000);
+window.addEventListener('load', function () {
+  document.body.classList.add('loaded_hiding');
+  window.setTimeout(function () {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  }, 1000);
+
+
+  let prev = document.querySelector('.arrow-left');
+  let next = document.querySelector('.arrow-right');
+  let slides = document.querySelectorAll('.slide');
+  let i = 0;
+  prev.onclick = function(){
+    slides[i].classList.remove('showed');
+      i--;
+      if(i < 0){
+          i = slides.length - 1;
+      }
+      slides[i].classList.add('showed');
+  };
+
+  next.onclick = function(){
+    slides[i].classList.remove('showed');
+      i++;
+      if(i >= slides.length){
+          i = 0;
+      }
+      slides[i].classList.add('showed');
   }
-
-
-
-$(document).ready(function(){
-    $('.review-slider').slick({
-        infinite: true,
-        arrows: true,
-        prevArrow: "<span class='arrow-left'><img src='img/arrow-left.png' alt='prev'></span>",
-        nextArrow: "<span class='arrow-right'><img src='img/arrow-right.png' alt='next'></span>",
-    });
 })
