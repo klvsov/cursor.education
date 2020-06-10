7af0e0ce49ecff240d924cd6b64e0a53301cb15c
klvsov <klvsov@gmail.com>
2020-06-10 11:10:17 +0300

    inserted youtube video

--- hw14/lib/removeVideo.js
+++
@@ -1,7 +0,0 @@
-let img = document.querySelector('#video img.alt_bg');
-let videoYt = document.querySelector('#video iframe');
-
-if(window.innerWidth <= 768){
-    img.classList.add('dBlock')
-    videoYt.remove();
-}
