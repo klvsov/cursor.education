15d57587c090397dcbbe6439cfa297d6ba95a196
klvsov <klvsov@gmail.com>
2020-07-10 15:42:34 +0300

    ready for level 2

--- assets/scripts/insertText.js
+++ assets/scripts/insertText.js
@@ -1,4 +1,4 @@
 let countWorks = document.querySelectorAll(".works-item");
 let textInChart = document.querySelector(".donut");
 
-textInChart.textContent = countWorks.length+"/14";
+textInChart.textContent = countWorks.length+"/21";
