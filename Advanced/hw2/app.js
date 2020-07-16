09c96b7e22a9866fd754d6213ad7036b17d505b9
klvsov <klvsov@gmail.com>
2020-07-16 22:53:51 +0300

    Conditions & Loops ready

---
+++ Advanced/hw2/app.js
@@ -0,0 +1,33 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const form = document.forms[0];
+    const firstNumber = form.elements.first;
+    const lastNumber = form.elements.last;
+    const skip = form.elements.isSkip;
+    const answer = document.querySelector('.answer');
+    const startCalc = document.getElementById('startCalc');
+
+    
+    startCalc.addEventListener('click', () => {
+        const start = parseInt(firstNumber.value);
+        const end = parseInt(lastNumber.value);
+        isSkipEven = skip.checked;
+        let sum = 0;
+
+        answer.classList.add('d-none');
+        
+        if(!isNaN(start) && !isNaN(end) && start < end) {
+            for(let i = start; i <= end; i++) {
+                if(isSkipEven && i % 2 === 0) {
+                    continue;
+                }
+                sum += i;
+            }
+            answer.textContent = `Answer: ${sum}`;
+            answer.classList.remove('d-none');
+            firstNumber.value = 0;
+            lastNumber.value = 0;
+            
+        }
+    })
+    
+})
