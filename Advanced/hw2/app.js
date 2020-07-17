94c3f4b05644bcd0a29675a013197cf49402e552
klvsov <klvsov@gmail.com>
2020-07-17 10:08:43 +0300

    fixed bug withgreater second number

--- Advanced/hw2/app.js
+++ Advanced/hw2/app.js
@@ -4,6 +4,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const lastNumber = form.elements.last;
     const skip = form.elements.isSkip;
     const answer = document.querySelector('.answer');
+    const error = document.querySelector('.error');
     const startCalc = document.getElementById('startCalc');
 
     
@@ -14,6 +15,12 @@ window.addEventListener('DOMContentLoaded', () => {
         let sum = 0;
 
         answer.classList.add('d-none');
+        error.classList.add('d-none');
+
+        if(start >= end) {
+            error.classList.remove('d-none');
+            error.textContent = ('Second number must be greater!');
+        }
         
         if(!isNaN(start) && !isNaN(end) && start < end) {
             for(let i = start; i <= end; i++) {
