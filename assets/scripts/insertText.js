44a43040b811c5a5833710e624f6d06eb14fb4c7
klvsov <klvsov@gmail.com>
2020-07-20 09:02:14 +0300

    updated main page for lesson 3

--- assets/scripts/insertText.js
+++ assets/scripts/insertText.js
@@ -1,4 +1,4 @@
 let countWorks = document.querySelectorAll(".works-item");
 let textInChart = document.querySelector(".donut");
 
-textInChart.textContent = countWorks.length+"/21";
+textInChart.textContent = countWorks.length+"/22 ("+Math.round((countWorks.length / 22) * 100)+'%)';
