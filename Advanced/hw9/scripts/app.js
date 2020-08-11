13f01f01c51b72b3e41fdb933bbbd0956f0307f9
klvsov <klvsov@gmail.com>
2020-08-11 09:20:36 +0300

    add clear interval

--- Advanced/hw9/scripts/app.js
+++ Advanced/hw9/scripts/app.js
@@ -2,35 +2,41 @@ window.addEventListener('DOMContentLoaded', () => {
     const startGenerateBlocks = document.querySelector('.generate-blocks');
     const setRandomColor = document.querySelector('.set-random-color');
     const blocksWrapper = document.querySelector('.wrapper');
-
+    const stop = document.querySelector('.stop-generate-color');
     const getRandNumber = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
 
     const generateBlocks = domEl => {
-        for (let i = 0; i < 25; i++) { // 25 - не магічна константа, а вимога умови домашнього завдання
-            domEl.innerHTML += `
+        if(!blocksWrapper.children.length) {
+            for (let i = 0; i < 25; i++) { // 25 - не магічна константа, а вимога умови домашнього завдання
+                domEl.innerHTML += `
                 <div class="block"></div>
             `
+            }
         }
     };
 
+    let intervalId;
+
     const generateBlocksInterval = () => {
-        if(!blocksWrapper.children.length){
-            generateBlocks(blocksWrapper);
-        }
+        generateBlocks(blocksWrapper);
         const blocks = document.querySelectorAll('.block');
         blocks.forEach(block => {
             block.style.backgroundColor = `rgb(${getRandNumber(0, 255)}, ${getRandNumber(0, 255)}, ${getRandNumber(0, 255)})`
         })
     };
 
-    startGenerateBlocks.addEventListener('click', () => {
-        if(!blocksWrapper.children.length){
-            generateBlocks(blocksWrapper);
-        }
+    startGenerateBlocks.addEventListener('click', (e) => {
+        e.target.setAttribute('disabled', 'disabled');
+        generateBlocks(blocksWrapper);
     });
 
-    setRandomColor.addEventListener('click', () => {
-        setInterval(generateBlocksInterval, 1000)
-    })
+    setRandomColor.addEventListener('click', (e) => {
+        e.target.setAttribute('disabled', 'disabled');
+        intervalId = setInterval(generateBlocksInterval, 1000);
+    });
 
+    stop.addEventListener('click', () => {
+        setRandomColor.removeAttribute('disabled');
+        clearInterval(intervalId);
+    });
 });
